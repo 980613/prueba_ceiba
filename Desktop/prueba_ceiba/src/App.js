901215a6abdc57f93d4988bdb9b5cc0048c3130b
Nicolas <nicolas1608outlook.com>
2022-01-06 10:08:40 -0500

    change

---
+++ Desktop/prueba_ceiba/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Create_Users from './Components/Create_Users';
+import LogIn from './Components/LogIn';
+import Users from './Components/Users';
+// hola mundo //
+const App = () => {
+  return (
+    <>
+      <Create_Users/>
+      <LogIn/>
+      <Users/>
+    </>
+  )
+}
+
+export default App;
